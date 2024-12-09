1542b3a33d17846af15f0d48a5a8221cd6d85d92
YagoGV <yagogv@gmail.com>
2024-12-09 12:38:20 +0100

    movidas comunes

---
+++ src/routes/api.routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+router.use('/users', require('./api/users.routes'));
+router.use('/attractions', require('./api/attractions.routes'));
+router.use('/comments', require('./api/comments.routes'));
+router.use('/schedules', require('./api/schedules.routes'));
+
+module.exports = router;
