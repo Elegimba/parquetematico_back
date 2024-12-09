9a1f6664a39a15feb5181e44c69d2989c79a8fff
DatSatoru <almarseda@gmail.com>
2024-12-09 17:30:59 +0100

    test .env

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -5,4 +5,5 @@ router.use('/attractions', require('./api/attractions.routes'));
 router.use('/comments', require('./api/comments.routes'));
 router.use('/schedules', require('./api/schedules.routes'));
 
+
 module.exports = router;
